a902299682a67bf5a45a0e0b8c8f3e7203dfd0d3
Elkhan Julian Brillianshah <ubbeg2000@gmail.com>
2022-09-28 21:34:25 +0700

    add: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,247 @@
+/*
+ * Copyright (c) 2020. Julian Steenbakker.
+ * All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ble_peripheral/flutter_ble_peripheral.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(const FlutterBlePeripheralExample());
+
+class FlutterBlePeripheralExample extends StatefulWidget {
+  const FlutterBlePeripheralExample({Key? key}) : super(key: key);
+
+  @override
+  FlutterBlePeripheralExampleState createState() =>
+      FlutterBlePeripheralExampleState();
+}
+
+class FlutterBlePeripheralExampleState
+    extends State<FlutterBlePeripheralExample> {
+  final FlutterBlePeripheral blePeripheral = FlutterBlePeripheral();
+
+  final AdvertiseData advertiseData = AdvertiseData(
+    serviceUuid: 'bf27730d-860a-4e09-889c-2d8b6a9e0fe7',
+    // serviceDataUuid: 'd02f6c5e-4d5c-4b6b-9ca4-9de2a9537220',
+    // localName: "DODODOR",
+    // serviceData: ,
+    includePowerLevel: true,
+    includeDeviceName: true,
+    // manufacturerId: 1234,
+    // serviceData: Uint8List.fromList([1, 2, 3, 4, 5, 6]),
+  );
+
+  final AdvertiseSettings advertiseSettings = AdvertiseSettings(
+    advertiseMode: AdvertiseMode.advertiseModeBalanced,
+    txPowerLevel: AdvertiseTxPower.advertiseTxPowerHigh,
+    timeout: 180000,
+    connectable: false,
+  );
+
+  final AdvertiseSetParameters advertiseSetParameters = AdvertiseSetParameters(
+    txPowerLevel: txPowerHigh,
+  );
+
+  bool _isSupported = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    final isSupported = await blePeripheral.isSupported;
+    setState(() {
+      _isSupported = isSupported;
+    });
+  }
+
+  Future<void> _toggleAdvertise() async {
+    if (await blePeripheral.isAdvertising) {
+    } else {
+      while (true) {
+        await blePeripheral.start(
+          advertiseData: advertiseData,
+          advertiseSettings: advertiseSettings,
+        );
+        sleep(const Duration(milliseconds: 500));
+        await blePeripheral.stop();
+      }
+    }
+  }
+
+  Future<void> _toggleAdvertiseSet() async {
+    if (await blePeripheral.isAdvertising) {
+      await blePeripheral.stop();
+    } else {
+      await blePeripheral.start(
+        advertiseData: advertiseData,
+        advertiseSetParameters: advertiseSetParameters,
+      );
+    }
+  }
+
+  Future<void> _requestPermissions() async {
+    final Map<Permission, PermissionStatus> statuses = await [
+      Permission.bluetooth,
+      Permission.bluetoothAdvertise,
+      Permission.location,
+    ].request();
+    for (final status in statuses.keys) {
+      if (statuses[status] == PermissionStatus.granted) {
+        debugPrint('$status permission granted');
+      } else if (statuses[status] == PermissionStatus.denied) {
+        debugPrint(
+          '$status denied. Show a dialog with a reason and again ask for the permission.',
+        );
+      } else if (statuses[status] == PermissionStatus.permanentlyDenied) {
+        debugPrint(
+          '$status permanently denied. Take the user to the settings page.',
+        );
+      }
+    }
+  }
+
+  final _messangerKey = GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scaffoldMessengerKey: _messangerKey,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter BLE Peripheral'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Is supported: $_isSupported'),
+              StreamBuilder(
+                stream: blePeripheral.onPeripheralStateChanged,
+                initialData: PeripheralState.unknown,
+                builder:
+                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  return Text(
+                    'State: ${describeEnum(snapshot.data as PeripheralState)}',
+                  );
+                },
+              ),
+              // StreamBuilder(
+              //     stream: blePeripheral.getDataReceived(),
+              //     initialData: 'None',
+              //     builder:
+              //         (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              //       return Text('Data received: ${snapshot.data}');
+              //     },),
+              Text('Current UUID: ${advertiseData.serviceUuid}'),
+              MaterialButton(
+                onPressed: _toggleAdvertise,
+                child: Text(
+                  'Toggle advertising',
+                  style: Theme.of(context)
+                      .primaryTextTheme
+                      .button!
+                      .copyWith(color: Colors.blue),
+                ),
+              ),
+              MaterialButton(
+                onPressed: _toggleAdvertiseSet,
+                child: Text(
+                  'Toggle advertising set for 1 second',
+                  style: Theme.of(context)
+                      .primaryTextTheme
+                      .button!
+                      .copyWith(color: Colors.blue),
+                ),
+              ),
+              StreamBuilder(
+                stream: blePeripheral.onPeripheralStateChanged,
+                initialData: PeripheralState.unknown,
+                builder: (
+                  BuildContext context,
+                  AsyncSnapshot<PeripheralState> snapshot,
+                ) {
+                  return MaterialButton(
+                    onPressed: snapshot.data == PeripheralState.poweredOff
+                        ? () async {
+                            final bool enabled = await blePeripheral
+                                .enableBluetooth(askUser: false);
+                            if (enabled) {
+                              _messangerKey.currentState!.showSnackBar(
+                                const SnackBar(
+                                  content: Text('Bluetooth enabled!'),
+                                  backgroundColor: Colors.green,
+                                ),
+                              );
+                            } else {
+                              _messangerKey.currentState!.showSnackBar(
+                                const SnackBar(
+                                  content: Text('Bluetooth not enabled!'),
+                                  backgroundColor: Colors.red,
+                                ),
+                              );
+                            }
+                          }
+                        : null,
+                    child: Text(
+                      'Enable Bluetooth (ANDROID)',
+                      style: Theme.of(context)
+                          .primaryTextTheme
+                          .button!
+                          .copyWith(color: Colors.blue),
+                    ),
+                  );
+                },
+              ),
+              MaterialButton(
+                onPressed: () async {
+                  final bool enabled = await blePeripheral.enableBluetooth();
+                  if (enabled) {
+                    _messangerKey.currentState!.showSnackBar(
+                      const SnackBar(
+                        content: Text('Bluetooth enabled!'),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+                  } else {
+                    _messangerKey.currentState!.showSnackBar(
+                      const SnackBar(
+                        content: Text('Bluetooth not enabled!'),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
+                },
+                child: Text(
+                  'Ask if enable Bluetooth (ANDROID)',
+                  style: Theme.of(context)
+                      .primaryTextTheme
+                      .button!
+                      .copyWith(color: Colors.blue),
+                ),
+              ),
+              MaterialButton(
+                onPressed: _requestPermissions,
+                child: Text(
+                  'Request Permissions',
+                  style: Theme.of(context)
+                      .primaryTextTheme
+                      .button!
+                      .copyWith(color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
